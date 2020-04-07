5860b53e4ab5c14b23e5199434f7b4c704e181a5
Meta-chan <k.sovailo@gmail.com>
2020-04-07 18:07:55 +0300

    Spelling error corrected

--- implementation/ir_fft_implementation.h
+++ implementation/ir_fft_implementation.h
@@ -4,7 +4,7 @@
 		- Please keep this notice and include the license file to your project
 		- I provide no warranty
 	To get help with installation, visit README
-	Created by Meta-chan, @k.sovailo@gmail.com
+	Created by @meta-chan, k.sovailo@gmail.com
 	Reinventing bicycles since 2020
 */
 
