34ac75595f908b5e19b67085a3b51c13c1dfa04a
Meta-chan <k.sovailo@gmail.com>
2020-05-15 23:06:17 +0300

    Vector added, natvis system introduced

---
+++ testing/ir_vector_test.cpp
@@ -0,0 +1,32 @@
+#define IR_IMPLEMENT
+#include <ir_vector.h>
+#include <stdio.h>
+#include <vector>
+
+template class ir::Vector <unsigned int>;
+
+int print(ir::Vector<unsigned int> vector)
+{
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		printf("%u ", vector[i]);
+	}
+	return 0;
+};
+
+int _main()
+{
+	std::vector<unsigned int> v(10);
+	ir::Vector<unsigned int> vector(10);
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		vector[i] = i;
+	}
+	print(vector);
+	return 0;
+};
+
+int main()
+{
+	return _main();
+};
